16b5b180afc2a6812891dfffb876376d9f5b7908
Daniel Dides <ddides@bridgephase.com>
2024-06-25 19:09:35 +0000

    cmd/deploy: update usage string

--- cmd/deploy/bigbang.go
+++ cmd/deploy/bigbang.go
@@ -17,19 +17,23 @@ import (
 var (
 	bigBangUse = `bigbang`
 
-	bigBangShort = i18n.T(`deploy big bang components to your cluster`)
+	bigBangShort = i18n.T(`Deploy bigbang components to your cluster`)
 
-	bigBangLong = templates.LongDesc(i18n.T(`Deploy big bang components to your cluster`))
+	bigBangLong = templates.LongDesc(i18n.T(`Deploy bigbang and optional bigbang addons to your cluster.
+		This command invokes the helm command, so arguments after -- are passed to the underlying helm command.
+
+		Note: deployment of bigbang requires Flux to have been deployed to your cluster. See "bbctl deploy flux" for more information.
+	`))
 
 	bigBangExample = templates.Examples(i18n.T(`
-	    # Deploy big bang product to your cluster
+	    # Deploy bigbang product to your cluster
 		bbctl deploy bigbang
 
 		# Deploy bigbang to a k3d development cluster
 		bbctl deploy bigbang --k3d
 
 		# Deploy bigbang with a helm overrides file. All arguments after -- are passed to the underlying helm command
-		bbctl deploy bigbang -- -f ../overrides/values.yaml
+		bbctl deploy bigbang -- -f ../path/to/overrides/values.yaml
 		`))
 )
 
@@ -49,7 +53,7 @@ func NewDeployBigBangCmd(factory bbUtil.Factory, streams genericIOOptions.IOStre
 	configClient, err := factory.GetConfigClient(cmd)
 	loggingClient.HandleError("error getting config client", err)
 	loggingClient.HandleError(
-		"error setting k3dflag",
+		"error setting k3d flag",
 		configClient.SetAndBindFlag(
 			"k3d",
 			false,
@@ -113,7 +117,7 @@ func deployBigBangToCluster(command *cobra.Command, factory bbUtil.Factory, stre
 
 	chartPath := getChartRelativePath(config, "chart")
 	helmOpts := slices.Clone(args)
-	loggingClient.Info(fmt.Sprintf("preparing to deploy big bang to cluster, k3d=%v", config.DeployBigBangConfiguration.K3d))
+	loggingClient.Info(fmt.Sprintf("preparing to deploy bigbang to cluster, k3d=%v", config.DeployBigBangConfiguration.K3d))
 	if config.DeployBigBangConfiguration.K3d {
 		loggingClient.Info("Using k3d configuration")
 		helmOpts = insertHelmOptForExampleConfig(config, helmOpts, "policy-overrides-k3d.yaml")
--- cmd/deploy/root.go
+++ cmd/deploy/root.go
@@ -13,9 +13,12 @@ import (
 var (
 	deployUse = `deploy`
 
-	deployShort = i18n.T(`Manage deployments of big bang components`)
+	deployShort = i18n.T(`Deploy bigbang components and preqrequisites`)
 
-	deployLong = templates.LongDesc(i18n.T(`Manage deployments of big bang components and prerequisites`))
+	deployLong = templates.LongDesc(i18n.T(`Deploy bigbang components and prerequisites.
+
+	Note: Before deploying bigbang, you must first deploy flux into the cluster. See "bigbang deploy flux --help" for more information.
+	`))
 
 	deployExample = templates.Examples(i18n.T(``))
 )
@@ -28,7 +31,19 @@ func NewDeployCmd(factory bbUtil.Factory, streams genericIOOptions.IOStreams) *c
 		Long:    deployLong,
 		Example: deployExample,
 		Run: func(cmd *cobra.Command, args []string) {
-			_, err := streams.Out.Write([]byte(fmt.Sprintln("Please provide a subcommand for deploy (see help)")))
+			subCommands := cmd.Commands()
+
+			var validCommands string
+			for i, subCmd := range subCommands {
+				validCommands += fmt.Sprintf("%s", subCmd.Use)
+				if i != len(subCommands)-1 {
+					validCommands += ", "
+				}
+			}
+			_, err := streams.Out.Write([]byte(fmt.Sprintf("error: must specify one of: %s\n\n", validCommands)))
+			factory.GetLoggingClient().HandleError("Unable to write to output stream", err)
+
+			err = cmd.Help()
 			factory.GetLoggingClient().HandleError("Unable to write to output stream", err)
 		},
 	}
