8723afc872f23bb6e3dd761c5030f7bf723261b9
Darrien Lee <lee_darrien@bah.com>
2024-12-11 19:47:43 +0000

    Resolve "Add compilation date as a constant in pipeline"

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,12 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . "${DIR}/get_dirs.sh"
 
+# Set custom date
+BUILD_DATE=$(date -u +%Y-%m-%dT%H:%M:%SZ)
+echo
+echo "Build Time: \"$BUILD_DATE\""
+
 # Build
-echo "Building in $PACKAGE_DIR..."
-go build -o "$PACKAGE_DIR/bin/$PACKAGE_NAME"
+echo "building..."
+REPO_DIR="repo1.dso.mil/big-bang/product/packages/bbctl"
+go build -o "$PACKAGE_DIR/bin/$PACKAGE_NAME" -ldflags "-X $REPO_DIR/static.buildDate=$BUILD_DATE"
